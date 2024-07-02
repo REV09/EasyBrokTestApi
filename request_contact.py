998b292e58847c2b32dac5f7e88718aa9d20986a
REV09 <hector.morelos.607@gmail.com>
2024-07-02 13:12:35 -0600

    fixed line 5 to respect pep8 rules

--- request_contact.py
+++ request_contact.py
@@ -2,7 +2,8 @@ import requests
 
 request_url = "https://api.stagingeb.com/v1/contact_requests?page=1&limit=20"
 api_key = 'l7u502p8v46ba3ppgvj5y2aad50lb9'
-request_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+request_user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/126.0.0.0 Safari/537.36')
 request_headers = {"accept": "application/json",
                    "X-Authorization": api_key,
                    "User-Agent": request_user_agent
